78636887d36d9623e5605b5194f8ff88730e0df5
sagazirobiul <gazisagor00000@gmail.com>
2021-02-08 22:29:46 +0600

    all file update donegit add .

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
-// Get Data form server
-const submit = document.getElementById('submit');
-submit.addEventListener('click', function () {
-const inputValue = document.getElementById('inputValue').value;
-const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`
-fetch(url)
-    .then(res => res.json())
-    .then(data => mealsData(data.meals));
+// Get Data form server //
+const searchMeal = () => {
+    const inputValue = document.getElementById('inputValue').value;
+    if (inputValue === '') {
+        alert('Please fill out the search box :)')
+        document.getElementById('mealsDiv').style.display = "none";
+    } else {
+        const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputValue}`
+        fetch(url)
+            .then(res => res.json())
+            .then(data => mealsData(data.meals))
+            .catch(error => displayErrorMessage())
+    }
+}
 
-})
 
-// Get Meals Data
+// Get Meals Data //
 const mealsData = meal => {
+    const mealsDiv = document.getElementById('mealsDiv')
+    mealsDiv.innerHTML = '';
+    document.getElementById('errorMessage').innerText = "";
     meal.forEach(eachMeal => {
-        const mealsDiv = document.getElementById('mealsDiv')
         const div = document.createElement('div');
         div.className = 'mealPack'
         const mealInfo = `
-        <div onclick="getDetail(${eachMeal.strMeal})">
+        <div onclick="getDetail('${eachMeal.strMeal}')">
             <img src="${eachMeal.strMealThumb}">
             <h2 class="mealName">${eachMeal.strMeal}</h2>
         </div>
@@ -24,4 +31,40 @@ const mealsData = meal => {
         div.innerHTML = mealInfo;
         mealsDiv.appendChild(div)
     });
+}
+
+
+//Get each meal data //
+const getDetail = (mealName) => {
+    document.getElementById("inputBox").style.display = "none";
+    document.getElementById("mealsDiv").style.display = "none";
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=${mealName}`;
+    fetch(url)
+        .then(res => res.json())
+        .then(data => eachMealDetail(data))
+}
+
+
+// Get each meal ingredientList and others //
+const eachMealDetail = eachMeal => {
+    const mealDetailArea = document.getElementById('mealDetailArea');
+    mealDetailArea.innerHTML = `
+    <img src="${eachMeal.meals[0].strMealThumb}">
+    <div class="ingredientList">
+        <h2 class="mealName">${eachMeal.meals[0].strMeal}</h2>
+        <ul>
+            <li>${eachMeal.meals[0].strIngredient1}</li>
+            <li>${eachMeal.meals[0].strIngredient2}</li>
+            <li>${eachMeal.meals[0].strIngredient3}</li>
+            <li>${eachMeal.meals[0].strIngredient4}</li>
+            <li>${eachMeal.meals[0].strIngredient5}</li>
+        </ul>
+    </div>
+    `
+}
+
+
+// Error Message //
+const displayErrorMessage = () => {
+    document.getElementById('errorMessage').innerText = "Sorry.!!! we don't have that meal :)";
 }
